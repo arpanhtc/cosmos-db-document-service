45db91f28af8c63d37f6bb961062ebbdb488b806
Arpan Shah <arpanvgm@live.in>
2026-05-09 19:37:48 +0530

    init

---
+++ Cosmos.Db.Document.Service/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Cosmos.Db.Document.Service.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
